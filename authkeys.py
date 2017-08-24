3cdddff8ec6749a357334d1ff23b15afb50b6fd1
nabeelsherazi <nabeelsherazi97@gmail.com>
2017-08-24 14:20:48 -0400

    added gitignore

--- authkeys.py
+++
@@ -1,7 +0,0 @@
-# Twitter auth details
-consumer_key = 'eimcELjV8NV1Uor5OtKX7BMYd'
-consumer_secret = 'YSgbJ4u4Hz669EG4J87od4NGIJtKIMwYUX0zjuRl6WyTo52EJu'
-access_token = '845462705863610370-EYNCrVXZd2CYgyysQUCywm7W1MlcN7Z'
-access_token_secret = 'agYyRPjwGgfMVEwtT3E7EWI5qYhln8jBZJ1H3S5jzZcYS'
-# Indicio auth details
-indicoio_key = 'a5151ddaebf2e934a2b892f650677b3b'
